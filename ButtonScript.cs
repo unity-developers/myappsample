64329bd328ab993cfb3d0a699eb23a4a2ce416e9
tamizhuser <deenabrave02@gmail.com>
2019-01-24 11:42:01 +0530

    `

---
+++ ButtonScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject TextPanel;
+    public bool ishide = false;
+    public bool isSpeake = false;
+    public Sprite Speak_Sprite, DontSpeak_Sprite;
+    public Button speaker_Btn;
+    public AudioSource audioSource;
+
+    bool play_1st_time = true;
+    // public GameObject ArCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TextPanelFunc()
+    {
+        if(ishide)
+        {
+
+            TextPanel.GetComponent<Animator>().SetBool("Hide", true);
+
+            audioSource.Stop();
+
+            // ArCamera.SetActive(true);
+            ishide = false;
+        }
+        else
+        {
+            TextPanel.GetComponent<Animator>().SetBool("Hide", false);
+            if(play_1st_time)
+            {
+                audioSource.Play();
+                play_1st_time = false;
+            }
+            else if (speaker_Btn.GetComponent<Image>().sprite == DontSpeak_Sprite)
+            {
+                audioSource.Pause();
+            }
+            else
+            {
+                audioSource.Play();
+            }
+            // ArCamera.SetActive(false);
+            ishide = true;
+        }
+    }
+    public void VolumeFunc()
+    {
+        if (isSpeake)
+        {
+            speaker_Btn.GetComponent<Image>().sprite = DontSpeak_Sprite;
+            audioSource.Pause();
+            isSpeake = false;
+        }
+        else
+        {
+            speaker_Btn.GetComponent<Image>().sprite = Speak_Sprite;
+            audioSource.Play();
+            isSpeake = true;
+        }
+
+    }
+}
---
+++ RotateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    public bool isTextVisible = false;
+
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * speed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * speed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX);
+
+        //transform.Rotate(Vector3.right, rotY);
+
+    }
+}
